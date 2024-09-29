a67b3b5f5d746b68ca3eecf99543e86522d0a05b
Jahangir Hossain <jahangir@combosoft.co.uk>
2024-09-29 17:55:04 +0600

    update api

--- src/helpers/apiUrl.js
+++ src/helpers/apiUrl.js
@@ -1,7 +1,5 @@
 /* eslint-disable */
 export const BASE_URL = "https://yourdomain.com/";
-// export const API_URL = "https://api.2ndafriendly.online/frontend";
-export const API_URL = "http://localhost:1338/frontend";
+export const API_URL = "https://api.2ndafriendly.online/frontend";
+// export const API_URL = "http://localhost:1338/frontend";
 export const IMAGE_URL = "https://api.2ndafriendly.online";
-
-
